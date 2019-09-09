3fd86479aecc777211fb6069d579c64f316c2a60
NormanLiana <liananorman@pursuit.org>
2019-09-09 17:28:07 -0400

    Initial Commit

---
+++ Song-Search-API-Keys-LabTests/Song_Search_API_Keys_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Song_Search_API_Keys_LabTests.swift
+//  Song-Search-API-Keys-LabTests
+//
+//  Created by Liana Norman on 9/9/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import XCTest
+@testable import Song_Search_API_Keys_Lab
+
+class Song_Search_API_Keys_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
