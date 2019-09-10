c0993d0d9489ee6d8ed4c8d643be4179f8943a91
NormanLiana <liananorman@pursuit.org>
2019-09-10 15:22:56 -0400

    adds extension of VC for TVDataSource and prepares to organize code with Marks

--- Song-Search-API-Keys-Lab/ViewController.swift
+++ Song-Search-API-Keys-Lab/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,3 +21,16 @@ class ViewController: UIViewController {
 
 }
 
+// MARK: Extensions
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
